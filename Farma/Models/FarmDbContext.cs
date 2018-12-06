d54fc45fe866aa5d8a901debb3d380a1d6246dbc
Michał Szewczak <michalszewczak2@gmail.com>
2018-12-06 11:35:41 +0100

    Added DB Context and connection string to appsettings.json

---
+++ Farma/Models/FarmDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Farma.Models
+{
+    public class FarmDbContext : DbContext
+    {
+        public FarmDbContext(DbContextOptions<FarmDbContext>
+            options) : base(options) { }
+
+        // public db set
+    }
+}
--- Farma/Models/MilkQuantity.cs
+++ Farma/Models/MilkQuantity.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Farma.Models
 {
@@ -9,11 +8,12 @@ namespace Farma.Models
     {
         [Key]
         public int      Id          { get; set; }
-
+        
         [ForeignKey("Animal")]
         public int      AnimalId    { get; set; }
+        public Animal Animal        { get; set; }
 
         public DateTime MilkingDate { get; set; } //data_dojenia
         public int      MilkAmount  { get; set; } //ilosc_mleka_l
-    }
+    }     
 }
